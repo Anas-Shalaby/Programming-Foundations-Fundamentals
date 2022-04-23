35effae0dd4ccf8f303782189e5d86a936e424e4
Anas Youssef <anasyoussef649@gmail.com>
2022-04-23 17:45:44 +0200

    The challenges of the course

---
+++ Conditional code/Task2.py
@@ -0,0 +1,15 @@
+# Asking the user about my favorite food
+
+food = input("What is my favourite food ?");
+
+# Check the answer
+if food == "Rice" :
+  print("Yep! So amazing !")
+else :
+  print("Yuck! That's not it!")
+
+print("Thanks for playing")
+
+
+
+
---
+++ Modular code/Task3.py
@@ -0,0 +1,8 @@
+# Prints out the name of my favourite city
+
+def favorite_city(name) :
+  print("One of my favourite cities is " , name)
+
+favorite_city("Benha")
+favorite_city("Cairo")
+favorite_city("Alexandria")
